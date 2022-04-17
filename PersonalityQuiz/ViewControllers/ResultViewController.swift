66b73e386486aa45d621c5efc80994333cc55621
Vadim Kislov <kislov.v@gmail.com>
2022-04-17 21:44:58 +0300

    feat: add showing result (probable animal)

--- PersonalityQuiz/ViewControllers/ResultViewController.swift
+++ PersonalityQuiz/ViewControllers/ResultViewController.swift
@@ -12,11 +12,35 @@ class ResultViewController: UIViewController {
     @IBOutlet var descriptionLabel: UILabel!
     
     var answers: [Answer]!
-    // 2. Определить наиболее часто встречающийся тип животного
-    // 3. Отобразить результат в соответствии с этим животным
 
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.hidesBackButton = true
+        
+        showResult()
+    }
+    
+    private func showResult() {
+        let animal = getProbableAnimal()
+        
+        animalLabel.text = animal != nil ? "Вы - \(animal!.rawValue)" : "Вы - доселе неизвестное науке существо"
+        descriptionLabel.text = animal?.definition ?? ""
+    }
+    
+    private func getProbableAnimal() -> Animal? {
+        var frequencyOfAnimals = [Animal: Int]()
+        
+        for answer in answers {
+            if let _ = frequencyOfAnimals[answer.animal] {
+                frequencyOfAnimals[answer.animal]! += 1
+            } else {
+                frequencyOfAnimals[answer.animal] = 1
+            }
+        }
+        
+        
+        let sortedFrequency = frequencyOfAnimals.sorted { $0.value > $1.value}
+        
+        return sortedFrequency.first?.key
     }
 }
